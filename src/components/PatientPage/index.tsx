dcfde41d337597eb4140b1909cac5f1295589399
Pavel <imran.pavel@gmail.com>
2025-10-03 14:17:12 +0300

    9.22

---
+++ src/components/PatientPage/index.tsx
@@ -0,0 +1,71 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import patients from "../../services/patients";
+import { isString } from "../../typeUtils";
+import { Gender, Patient } from "../../types";
+import { Box, SvgIcon, Typography } from "@mui/material";
+import { Male, Female, Transgender } from '@mui/icons-material';
+
+type GenderProps = {
+    gender: Gender
+};
+
+const GenderIcon = (props: GenderProps) => {
+    const { gender } = props;
+
+    let genderIcon;
+
+    if(gender === Gender.Male) {
+        genderIcon = Male;
+    }
+    else if(gender === Gender.Female) {
+        genderIcon = Female;
+    }
+    else if(gender === Gender.Other) {
+        genderIcon = Transgender;
+    }
+    else {
+        genderIcon = null;
+    }
+
+    if(genderIcon) {
+        return (
+            <SvgIcon component={genderIcon}/>
+        );
+    }
+    return null;
+};
+
+
+const PatientPage = () => {
+
+    const params = useParams();
+    const [patient, setPatient] = useState<Patient>();
+    
+    useEffect(() => {
+        const patientId = params.id;
+        if(isString(patientId)) {
+            (async () => {
+                const fetchedPatient = await patients.getOne(patientId);
+                setPatient(fetchedPatient);
+            })();
+        }
+    }, [params.id]);
+
+    if(!patient) {
+        return null;
+    }
+
+    return (
+        <Box sx={{ marginTop: '50px' }}>
+            <Box sx={{ display: 'flex', justifyContent: 'flex-start', alignItems: 'center', gap: '5px' }}>
+                <Typography variant="body1"><strong>{patient.name}</strong></Typography>
+                <GenderIcon gender={patient.gender}/>
+            </Box>
+            <Typography variant="body1">SSN: {patient.ssn}</Typography>
+            <Typography variant="body1">Occupation: {patient.name}</Typography>
+        </Box>
+    );
+};
+
+export default PatientPage;
---
+++ src/typeUtils.ts
@@ -0,0 +1,11 @@
+
+
+export const isString = (text: unknown): text is string => {
+    return (typeof text === 'string') || (text instanceof String);
+};
+
+const typeUtils = {
+    isString
+};
+
+export default typeUtils;
